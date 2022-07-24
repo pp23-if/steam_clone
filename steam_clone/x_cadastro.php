720667c32b7bbc07388954fe9bbcd4ad9903c973
pp23-if <pedrouberaba23@gmail.com>
2022-07-24 12:50:28 -0300

    fiz o back end

--- steam_clone/x_cadastro.php
+++ steam_clone/x_cadastro.php
@@ -28,22 +28,23 @@
     </header>
     <script src="mobile-navbar.js"></script>
 
+   
 
 
     <!--conteúdo-->
 
     <x>
 
-    <form>
+    <form method="POST">
     
       
       <input type="text" class="inp" name="nome" id="nome" placeholder="nome" required maxlength="30">
     
      
-      <input type="number" class="inp" name="codigo" id="codigo" placeholder="informe 4 digitos quaisquer" required min="0000" max="9999">
+      <input type="number" class="inp" name="codigo" id="codigo" placeholder="informe 4 digitos quaisquer" required min="1111" max="9999">
     
        
-      <input type="email"  class="inp" name="email" id="email" placeholder="email" required maxlength="50">
+      <input type="email"  class="inp" name="email" id="email" placeholder="email" required maxlength="30">
       
   
       <input type="number" class="inp" name="idade" id="idade" placeholder="idade" required min="18" max="100">
@@ -55,10 +56,81 @@
     
        </form>
       </x>
+
       <br>
       <br>
       <br>
       <br>
+
+      <!-- BACK END --->
+
+
+      <?php
+      /*header('location: lista_download.html');
+      redirecionar pagina apos validação.*/
+       
+       $mensagem = "";
+
+
+         $nome = "";
+         $codigo = "";
+         $email = "";
+         $idade = "";
+         $jogo = "";
+
+         if(isset($_POST["nome"], $_POST["codigo"], $_POST["email"], $_POST["idade"], $_POST["jogo"]))
+         {
+           
+           $conexao = new PDO("mysql:host=localhost;dbname=steam_clone", "root", "",
+           array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+          
+        
+
+           $nome = filter_input(INPUT_POST, "nome", FILTER_UNSAFE_RAW);
+           $codigo = filter_input(INPUT_POST, "codigo", FILTER_SANITIZE_NUMBER_INT);
+           $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
+           $idade = filter_input(INPUT_POST, "idade", FILTER_VALIDATE_INT);
+           $jogo = filter_input(INPUT_POST, "jogo", FILTER_UNSAFE_RAW);
+          
+
+
+           if(!$nome || !$codigo || !$email || !$idade || !$jogo)
+           {
+             $mensagem = "dados invalidos";
+           }
+          else
+          {
+            $stm = $conexao->prepare('INSERT INTO usuario (nome,codigo,email,idade,jogo) VALUES (:nome,:codigo,:email,:idade,:jogo)');
+            $stm->bindParam('nome',$nome);
+            $stm->bindParam('codigo',$codigo);
+            $stm->bindParam('email',$email);
+            $stm->bindParam('idade',$idade);
+            $stm->bindParam('jogo',$jogo);
+            $stm->execute();
+
+          
+            $nome = "";
+            $codigo = "";
+            $email = "";
+            $idade = "";
+            $jogo= "";
+
+             header('location: lista_download.html');
+
+            
+          }
+
+
+
+         }
+         
+
+
+
+      ?>
+
+
+
 
 
     <!--RODAPÉ (footer)-->
